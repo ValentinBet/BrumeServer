66e3b3834cfc2627ed3fa4f785f82ea64fe5a189
ETU\v.betrancourt <valentin.betrancourt@hotmail.fr>
2020-10-05 17:00:55 +0200

    Init

---
+++ BrumeServer.cs
@@ -0,0 +1,22 @@
+﻿using DarkRift.Server;
+using DarkRift;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BrumeServer
+{
+    public class BrumeServer : Plugin
+    {
+        public BrumeServer(PluginLoadData pluginLoadData) : base(pluginLoadData)
+        {
+
+        }
+
+        public override bool ThreadSafe => false;
+
+        public override Version Version => new Version(1, 0, 0);
+    }
+}
